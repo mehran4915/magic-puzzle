de3968f5add4bceee7aeb633e17084eba2b4d8b9
Mehran <mehan4915@gmail.com>
2025-01-05 07:40:16 +0330

    create main menu

--- main.c
+++ main.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
 
-int main(){
+enum GameStatus { CONTINUE, STOP };
 
+int main() {
+  enum GameStatus game;
+  game = CONTINUE;
+  while (game == CONTINUE) {
+    int main_menu_option;
+    while (1) {
+      printf("Welcome to magic puzzle game\nplease choose an option\noption 1: "
+             "start\noption 2: manual\noption 3: exit\n-> ");
+      scanf("%d", &main_menu_option);
+      if (main_menu_option == 1 || main_menu_option == 2 ||
+          main_menu_option == 3) {
+        break;
+      } else {
+        printf("enter a valid number between 1 and 3");
+      }
+    }
+    if (main_menu_option == 2) {
+      printf("this is the manual\n");
+      continue;
+    }
+    if (main_menu_option == 3) {
+      printf("See you later....");
+      break;
+    }
+  }
+
+  return 0;
 }
